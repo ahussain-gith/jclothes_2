c4d35022710155ab26d196ab347649f525505702
Ahmad Hussain <a.hussain1301@gmail.com>
2020-01-11 16:34:38 +0400

    added diredtory menu

---
+++ src/components/header/header.component.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './header.styles.scss';
+
+const Header = () =>(
+    <div className="header">
+    <div className="logo">
+        LOGO
+    </div>
+    <div className="options">
+        <div className="option">
+            Shop
+        </div>
+        <div className="option">
+            Contact
+        </div>
+        <div className="option">
+            Sign In
+        </div>
+    </div>
+
+    </div>
+);
+
+export default Header;
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import "./homepage.styles.scss";
-import Collections from "../../components/collections/collections.component";
+//import Collections from "../../components/collections/collections.component";
+import Directory from "../../components/directory/directory.component";
+import Header from "../../components/header/header.component";
 
 const HomePage = () => (
   <div className="homepage">
-    <Collections />
+    <Header />
+    <Directory />
   </div>
 );
 export default HomePage;
